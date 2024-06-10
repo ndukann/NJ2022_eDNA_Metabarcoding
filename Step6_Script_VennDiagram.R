0100c83a06c8963840df7ebe4e612cf1082b2a5b
ndukann <nergiz.dukan@ilvo.vlaanderen.be>
2024-06-10 10:37:18 +0200

    Update Step6_Script_VennDiagram.R

--- Step6_Script_VennDiagram.R
+++ Step6_Script_VennDiagram.R
@@ -42,9 +42,6 @@ eDNA_species_b <- rownames(samples_fish_bb)
 colnames(data_morph)[colnames(data_morph) == "Pomatoschistus"] <- "Pomatoschistus sp."
 morph_species <- c(colnames(data_morph))
 
-counts1 <- c(length(eDNA_species_s), length(eDNA_species_b), length(unique(morph_species)))
-counts1
-
 pdf("Venn_numbers_NJ2022_MicroDecon.pdf",width=20,height=18,bg="white")
 
 v <- venn.diagram(x=list(eDNA_species_s, eDNA_species_b, morph_species), category.names = c("eDNA Surface", "eDNA Bottom", "Morphology"),
@@ -67,27 +64,7 @@ v[[10]]$label <- paste(intersect(eDNA_species_s, intersect(eDNA_species_b, morph
 v[[9]]$label <- paste(setdiff(intersect(eDNA_species_b, morph_species), eDNA_species_s), collapse="\n")
 v[[7]]$label <- paste(setdiff(morph_species, union(eDNA_species_s, eDNA_species_b)), collapse="\n")
 
-# v[[10]]$label <- paste(setdiff(intersect(eDNA_species_s, morph_species), eDNA_species_b), collapse="\n")
-
 grid.newpage(v)
 grid.draw(v)
 
-
 save.image((paste0(proj.path,"/02_NJ2022_Miseq_5119441/R_Environment/R_Environment_MicroDecon/R_Environment_VennDiagram_MicroDecon.RData")))
-
-species <- c("Cyclopterus lumpus", "Diplecogaster bimaculata", "Gasterosteus aculeatus", "Lophius piscatorius", "Pomatoschistus pictus", "Zeugopterus regius", "Anguilla anguilla", "Neogobius melanostomus", "Salmo salar", "Scophthalmus rhombus", "Trisopterus minutus", "Atherina presbyter", "Buglossidium luteum", "Trachinus draco")
-species_sb <- as.data.frame(community_NJ2022[rownames(community_NJ2022) %in% species,])
-a <- as.data.frame(community_NJ2022[grep("anguilla", rownames(community_NJ2022)),])
-a <- as.data.frame(a[,!a==0])
-
-ReadCounts <- as.data.frame(rowSums(species_sb[1:nrow(species_sb),])) #number of reads
-names(ReadCounts)<- "Read_Counts"
-Samples <- as.data.frame(rowSums(species_sb !=0)) #number of samples
-names(Samples)<- "Sample_Counts"
-
-table_species_sb <- cbind(ReadCounts, Samples)
-
-write.xlsx(table_species_sb, 
-           paste0(proj.path,"/02_NJ2022_Miseq_5119441/NJ2022_Taxonomic_Assignment_Tables/Results_MicroDecon/Species_Only_Surface_Bottom.xlsx"), 
-           sheetName = "Sheet1", colNames = TRUE, rowNames = TRUE, append = FALSE)
-
